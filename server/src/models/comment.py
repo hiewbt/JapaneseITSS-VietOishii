e2fb1540b5c3397dc9d467f238f5a4b256165143
havisdino <hadinh893@gmail.com>
2024-12-26 16:10:09 +0700

    Add comment ID

--- server/src/models/comment.py
+++ server/src/models/comment.py
@@ -2,10 +2,12 @@ from . import db
 
 
 class Comment(db.Model):
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), primary_key=True)
-    dish_id = db.Column(db.Integer, db.ForeignKey("dish.id"), primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    dish_id = db.Column(db.Integer, db.ForeignKey("dish.id"))
     content = db.Column(db.String(1000), nullable=False)
     stars = db.Column(db.Integer, nullable=False)
+    img_url = db.Column(db.String(500))
     
     def to_dict(self):
         return {
